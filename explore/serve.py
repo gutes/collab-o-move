7191f87fac0902141ed81319dac7cbe63b91c769
fruss <fernando.russ@gmail.com>
2011-04-03 23:18:15 -0300

    * added PoC server-side translation of acc. data   from m/s^2 to an angle in degrees. * removed some nasty round code from javascript code,   in Kaos we consume floats.

--- explore/serve.py
+++ explore/serve.py
@@ -2,6 +2,7 @@ import tornado
 import tornado.web
 import tornado.websocket
 import tornado.httpserver
+import math
 
 # command line options
 from tornado.options import define, options
@@ -12,7 +13,6 @@ define("host", default="127.0.0.1", help="run on the given ip", type=str)
 # main handler
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        
         self.render("content.html", 
                     websocket_server_host=options.host,
                     websocket_server_port=options.port)
@@ -24,9 +24,10 @@ class EchoWebSocket(tornado.websocket.WebSocketHandler):
     def open(self):
         print "WebSocket opened"
 
-    def on_message(self, message):
-        print "acceleration-data:", message
-        #self.write_message(u"You said: " + message)
+    def on_message(self, data):
+        ax, ay, az, ai, arAlpha, arBeta, arGamma = data.split(":")
+        angle = math.atan2( float(ay), float(ax) ) * 57.29577951308232
+        print "acceleration-data: angle=%d (ax=%s ay=%s az=%s)" % (angle, ax,ay,az)
 
 
     def on_close(self):
