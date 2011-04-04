1e7ec9219f81383f7c38dd2171a0a71a36b9662d
fruss <fernando.russ@gmail.com>
2011-04-03 22:24:38 -0300

    * big f..ck commit  * now we we use Tornado  * prototype using WebSockets to comunicate between the device and the server (low latency!)  * mmmm... nice...

--- explore/serve.py
+++ explore/serve.py
@@ -1,21 +1,47 @@
-import cherrypy
+import tornado
+import tornado.web
+import tornado.websocket
+import tornado.httpserver
 
-class MyApp:
-    """ Sample request handler class. """
-
-    def index(self):
-        with open("content.html") as content:
-            return content.read()
-
-    index.exposed = True
-
-
-    def exit(self):
-        raise SystemExit(0)
-    exit.exposed = True
+# command line options
+from tornado.options import define, options
+define("port", default=8888, help="run on the given port", type=int)
+define("host", default="127.0.0.1", help="run on the given ip", type=str)
 
 
-cherrypy.config.update({'server.socket_host': '0.0.0.0',
-                        'server.socket_port': 8080,
-                       })
-cherrypy.quickstart( MyApp() )
+# main handler
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        
+        self.render("content.html", 
+                    websocket_server_host=options.host,
+                    websocket_server_port=options.port)
+
+#
+# websocket handler
+#
+class EchoWebSocket(tornado.websocket.WebSocketHandler):
+    def open(self):
+        print "WebSocket opened"
+
+    def on_message(self, message):
+        print "acceleration-data:", message
+        #self.write_message(u"You said: " + message)
+
+
+    def on_close(self):
+        print "WebSocket closed"
+
+    
+def main():
+    tornado.options.parse_command_line()
+    application = tornado.web.Application([
+        (r"/", MainHandler),
+        (r"/websocket",EchoWebSocket)
+    ])
+    http_server = tornado.httpserver.HTTPServer(application)
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
+                
+if __name__ == "__main__":
+    main()
